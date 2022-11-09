951b9c54a040509ada0977eb95fba09d7a9fe0b7
ZohalZShukoor <zohal.shukoor@outlook.com>
2022-11-08 17:19:23 -0800

    recent changes

--- src/gitFirst/FirstGit.java
+++ src/gitFirst/FirstGit.java
@@ -5,7 +5,7 @@ public class FirstGit {
 	public void main (String [] args) {
 		System.out.println("This is a test");
 	}
-	public void main (String [] args) {
+	public void remote (String [] args) {
 		System.out.println("this is from remote repo");
 	}
 
