617b1e1788b61298b21b67902c84445c8f66301e
ZohalZShukoor <117646222+ZohalZShukoor@users.noreply.github.com>
2022-11-08 17:15:46 -0800

    Update FirstGit.java
    this is from remote repo

--- src/gitFirst/FirstGit.java
+++ src/gitFirst/FirstGit.java
@@ -5,5 +5,8 @@ public class FirstGit {
 	public void main (String [] args) {
 		System.out.println("This is a test");
 	}
+	public void main (String [] args) {
+		System.out.println("this is from remote repo");
+	}
 
 }
